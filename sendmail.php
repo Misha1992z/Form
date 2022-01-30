a25c5abe39746c5df0f493d3aedf3e5f75817bbc
Misha1992z <89654675+Misha1992z@users.noreply.github.com>
2022-01-30 13:10:33 +0500

    form

--- sendmail.php
+++ sendmail.php
@@ -11,7 +11,7 @@
 	$mail->IsHTML(true);
 
 	//От кого письмо
-	$mail->setFrom('misha1992z.github.io, 'Фрилансер по жизни');
+	$mail->setFrom('https://misha1992z.github.io/Form/, 'Фрилансер по жизни');
 	//Кому отправить
 	$mail->addAddress('zubovm1992@gmail.com');
 	//Тема письма
